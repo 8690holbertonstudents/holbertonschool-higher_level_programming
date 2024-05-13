97510ac993bd92c62cac7557bd4202afad0c8849
8690holbertonstudents <8690@holbertonstudents.com>
2024-05-13 12:05:32 +0200

    Correction T1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    print(len(my_list))
     if idx < 0:
         return
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return
     else:
         return(my_list[idx])
