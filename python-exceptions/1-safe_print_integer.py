7682b52055364e20538fb0eb1018b7f73daf2392
8690holbertonstudents <8690@holbertonstudents.com>
2024-05-15 13:55:08 +0200

    Correction 2 Task 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    if value is None:
+        return (False)
     try:
         if isinstance(value, int):
             print("{:d}".format(value))
             return (True)
-    except Exception:
+    except TypeError:
         return (False)
 
