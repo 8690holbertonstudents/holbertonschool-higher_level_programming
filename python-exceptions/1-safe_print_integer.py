6d3f879ad4bcee0d775626b4a56ad068b625a757
8690holbertonstudents <8690@holbertonstudents.com>
2024-05-15 13:51:11 +0200

    Correction 1 Task 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,5 +4,6 @@ def safe_print_integer(value):
         if isinstance(value, int):
             print("{:d}".format(value))
             return (True)
-    except TypeError:
+    except Exception:
         return (False)
+
