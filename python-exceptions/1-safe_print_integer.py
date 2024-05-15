3bd766f37fa542c33153c4c277f716f5c8b6c6d0
8690holbertonstudents <8690@holbertonstudents.com>
2024-05-15 14:25:23 +0200

    Correction 3 Task 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -6,5 +6,5 @@ def safe_print_integer(value):
         if isinstance(value, int):
             print("{:d}".format(value))
             return (True)
-    except TypeError:
+    except TypeError or ValueError:
         return (False)
