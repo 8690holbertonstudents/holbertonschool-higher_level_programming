3a27254f260beb459961b138ec4e28ddcc6aa978
8690holbertonstudents <8690@holbertonstudents.com>
2024-05-16 14:50:50 +0200

    Correction 3 Task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python3
 """
 This Python script defines a class named Square.
-
-Currently, the class definition is empty (`pass` statement). 
-
-You can extend this class to model a square shape with attributes and methods 
-specific to squares, such as:
-
-* Private attribute: `size` to represent the side length of the square.
-* Methods:
-    * `__init__(self, size)`: Constructor to initialize the square with a size.
-    * `area(self)`: Method to calculate the area of the square.
-    * `perimeter(self)`: Method to calculate the perimeter of the square.
-    * `get_size(self)`: Method to get the current size of the square.
-    * `set_size(self, new_size)`: Method to set a new size for the square 
-      (with potential validation or error handling).
-
-By adding these functionalities, you can create a Square object and use its methods 
-to perform calculations and operations related to squares.
 """
+
+
 class Square:
-    pass
+    pass  # An empty block
