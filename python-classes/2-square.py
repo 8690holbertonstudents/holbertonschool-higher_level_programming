bcb0fe9a980041a0d7be0010d44b6dfe9294eca0
8690holbertonstudents <8690@holbertonstudents.com>
2024-05-17 09:03:08 +0200

    Push Task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
+"""Python script to define class.
+"""
+
+
 class Square:
+    """Create a class named Square.
+    """
     def __init__(self, size=0):
+        """Initialize the Square object.
+        Args:
+            size: Size of the the object Square.
+
+        Raises:
+            TypeError: size must be an integer.
+            ValueError: size must be upper or equal to zero.
+        """
         self.__size = size
         if isinstance(self.__size, int) is False:
-            raise ValueError("size must be an integer")
+            raise TypeError("size must be an integer")
         elif self.__size < 0:
-            raise TypeError("size must be >= 0")
+            raise ValueError("size must be >= 0")
