806f36fde74ce52488af74c4fa16fd49ecc516c8
8690holbertonstudents <8690@holbertonstudents.com>
2024-05-17 08:43:29 +0200

    Push Task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Python script to define class.
+"""
+
+
 class Square:
+    """Create a class named Square.
+    """
     def __init__(self, size):
+        """Initialize the Square object.
+        Args:
+            size: Size of the the object Square.
+        """
         self.__size = size
