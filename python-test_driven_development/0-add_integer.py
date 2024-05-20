579996b4eb141379da21edd5050d3eb4c212f030
8690holbertonstudents <8690@holbertonstudents.com>
2024-05-20 14:37:31 +0200

    Push mandatory files + Task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Python script for using doctest.
+    Using a .txt file in /tests directory.
+
+"""
+import doctest
+"""doctest.testfile("example.txt")"""
+
+
+def add_integer(a, b=98):
+    """
+        Function to add two integers.
+    """
+    if not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
