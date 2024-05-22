5f7dc518725eba60b361b794fc25fdb3442471dd
8690holbertonstudents <8690@holbertonstudents.com>
2024-05-23 00:11:14 +0200

    Files for T7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+    Module to manipulate subclasses.
+"""
+
+
+class BaseGeometry:
+    """
+        Empty class named BaseGeometry.
+    """
+    def area(self):
+        """ function to calculate the area of BaseGeometry.
+
+        Returns:
+            Return exception cause area is not implemented.
+
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Function to validate argument value.
+
+        Args:
+            value: Verify if value is an integer..
+
+        Raises:
+            TypeError: value must be an integer.
+            ValueError: value must be upper than zero.
+
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
