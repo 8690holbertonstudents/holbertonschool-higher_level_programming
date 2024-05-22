ce9b2309697f73de008f3d26bd54a19f3750ad36
8690holbertonstudents <8690@holbertonstudents.com>
2024-05-22 16:53:09 +0200

    Modify T2 (PEP8)

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -16,7 +16,7 @@ def is_same_class(obj, a_class):
         Returns:
         True if exact, otherwise False.
     """
-    if type(obj) == a_class:
+    if type(obj) is not a_class:
         return (True)
     else:
         return (False)
