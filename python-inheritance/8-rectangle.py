767dc3cfd29c8a03aeb89c334e9741b0ca64c87b
8690holbertonstudents <8690@holbertonstudents.com>
2024-05-23 15:05:44 +0200

    Files for T8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Module to manipulate subclasses.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+        Subclass Rectangle inherit from class BaseGeometry.
+
+        Args:
+            width: width of subclass Rectangle.
+            height: height of subclass Rectangle.
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+        self.integer_validator("width",width)
+        self.integer_validator("height",height)
