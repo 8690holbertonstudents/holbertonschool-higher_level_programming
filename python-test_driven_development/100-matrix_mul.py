73348b372337a00b8a96540c1f70d431884f0320
8690holbertonstudents <8690@holbertonstudents.com>
2024-05-26 22:00:05 +0200

    Modify files T6

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -12,8 +12,8 @@ def matrix_mul(m_a, m_b):
 
     if not all(isinstance(row_a, list) for row_a in m_a) \
        or not all(isinstance(row_b, list) for row_b in m_b):
-        raise TypeError("m_a must be a list of \
-                         lists or m_b must be a list of lists")
+        raise TypeError(
+            "m_a must be a list of lists or m_b must be a list of lists")
 
     if len(m_a) == 0 or all(len(row_a) == 0 for row_a in m_a) \
        or len(m_b) == 0 or all(len(row_b) == 0 for row_b in m_b):
@@ -31,10 +31,11 @@ def matrix_mul(m_a, m_b):
             if not isinstance(num, (int, float)):
                 raise TypeError(msg1 + msg2)
 
+    msg3 = "each row of m_a must be of the same "
+    msg4 = "size or each row of m_b must be of the same size"
     if any(len(row_a) != len(m_a[0]) for row_a in m_a) \
        or any(len(row_b) != len(m_b[0]) for row_b in m_b):
-        raise TypeError("each row of m_a must be of the same \
-                         size or each row of m_b must be of the same size")
+        raise TypeError(msg3 + msg4)
 
     if not (len(m_a[0]) == len(m_b)):
         raise ValueError("m_a and m_b can't be multiplied")
