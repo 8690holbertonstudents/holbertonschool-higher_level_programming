f0ccf9cdb49bd9de5aa573f5ccadc14f0103a73e
8690holbertonstudents <8690@holbertonstudents.com>
2024-05-27 14:46:56 +0200

    Python - File I/O - T5 - Correction 1

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -18,4 +18,4 @@ def save_to_json_file(my_obj, filename):
          Write the object JSON string to a text file.
     """
     with open(filename, mode="w", encoding="utf-8") as My_File:
-        read_data = My_File.write(json.dumps(my_obj))
+        read_data = My_File.write(json.dumps(my_obj, sort_keys=True))
