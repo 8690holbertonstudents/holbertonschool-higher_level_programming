2769c5a615171314030e6e079e788b1819b6b6e5
8690holbertonstudents <8690@holbertonstudents.com>
2024-05-27 11:01:04 +0200

    Python - I/O - T1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,6 +7,12 @@
 def read_file(filename=""):
     """
         Function to read a text file (UTF8) and prints to stdout.
+
+        Args:
+         filename: The file to open.
+
+        Returns:
+         The entire file content.
     """
     with open(filename, mode="r", encoding="utf-8") as My_File:
         read_data = My_File.read()
---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Module to write to a text file.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+        Function to write to  a text file (UTF8).
+
+        Args:
+         filename: The filename to create.
+         text: The to write in the file.
+
+        Returns:
+         The numbers of characters written.
+    """
+    with open(filename, mode="w", encoding="utf-8") as My_File:
+        read_data = My_File.write(text)
+        return (len(text))
