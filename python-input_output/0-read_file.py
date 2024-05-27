050427aa5904e6f007fb14c024fdf4876e625133
8690holbertonstudents <8690@holbertonstudents.com>
2024-05-27 10:46:55 +0200

    Python - Input/Output - T0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Module to read from a text file.
+"""
+
+
+def read_file(filename=""):
+    """
+        Function to read a text file (UTF8) and prints to stdout.
+    """
+    with open(filename, mode="r", encoding="utf-8") as My_File:
+        read_data = My_File.read()
+        print(read_data, end="")
