24fb21f5aff9e3d859b2263d31bda391d432fb6d
8690holbertonstudents <8690@holbertonstudents.com>
2024-05-27 11:44:18 +0200

    Python - File I/O - T5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Module to use JSON.
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+        Function that writes an Object to a text file,
+        using a JSON representation.
+
+        Args:
+         my_obj: The object to represent in JSON string.
+         filename: The text file to write in.
+
+        Returns:
+         Write the object JSON string to a text file.
+    """
+    with open(filename, mode="w", encoding="utf-8") as My_File:
+        read_data = My_File.write(json.dumps(my_obj))
