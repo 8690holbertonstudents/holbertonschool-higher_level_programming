bce15c905bfcf95d72b3b1fd8cc2af9d4989f4de
8690holbertonstudents <8690@holbertonstudents.com>
2024-06-01 19:43:37 +0200

    Python - RestfulAPI - T3 - Corr 2

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -36,11 +36,16 @@ class MyFirstServer(http.server.BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(dataset).encode("utf-8"))
         elif self.path == "/info":
             self.send_response(200)
-            self.send_header("Content-type", "text/html")
+            self.send_header("Content-type", "application/json")
             self.end_headers()
             dataset = {"version": "1.0",
                        "description": "A simple API built with http.server"}
             self.wfile.write(json.dumps(dataset).encode("utf-8"))
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("OK", "utf-8"))
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/html")
