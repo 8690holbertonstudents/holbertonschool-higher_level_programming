5ad246bb67d4cebc5eaba3033a1baefc4acfc549
8690holbertonstudents <8690@holbertonstudents.com>
2024-06-19 19:02:42 +0200

    SQL More queries - Correction (2) T15

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -4,7 +4,6 @@
 -- each record should display: tv_shows.title
 -- results sorted in ascending order by the show title
 -- database passed as arg to mysql cli
-USE hbtn_0d_tvshows;
 SELECT tv_shows.title
   FROM tv_shows
  INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
