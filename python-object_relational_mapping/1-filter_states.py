cf13f3c7792a31159624cb47e4a61bc1c601ccdb
8690holbertonstudents <8690@holbertonstudents.com>
2024-06-21 09:05:05 +0200

    SQL ORM - T1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Python module that lists all states with a name
+starting with N (upper N) from the database hbtn_0e_0_usa
+Code should not be executed when imported
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         password=password, database=database)
+    cur = db.cursor()
+    cur.execute(
+        """SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC""")
+
+    rows = cur.fetchall()
+    for item in rows:
+        print(item)
+
+    cur.close()
+    db.close()
