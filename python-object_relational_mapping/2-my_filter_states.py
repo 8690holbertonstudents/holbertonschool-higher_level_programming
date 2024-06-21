35fea1f26b2f7dbb29192c27a04a85b39f0de93c
8690holbertonstudents <8690@holbertonstudents.com>
2024-06-21 09:52:36 +0200

    SQL ORM - T2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Python module that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    searched = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         password=password, database=database)
+    cur = db.cursor()
+    cur.execute(
+        """SELECT * FROM states WHERE name = "{}" ORDER BY id ASC""".format(searched))
+
+    rows = cur.fetchall()
+    for item in rows:
+        print(item)
+
+    cur.close()
+    db.close()
