7bb0c101d3fd2dd6e5063d0cdf97fbf9ab16c2db
8690holbertonstudents <8690@holbertonstudents.com>
2024-06-21 22:47:51 +0200

    SQL ORM - T7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Python module that lists all State objects from the database hbtn_0e_6_usa
+Usinng module SQLAlchemy
+Import State and Base from the model_state module
+"""
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    connection_string = (
+        f"mysql+mysqlconnector://{username}:{password}@localhost:3306/{database}")
+    engine = create_engine(connection_string, echo=False)
+
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        query = session.query(State).order_by(State.id).all()
+        for state in query:
+            print(f"{state.id}: {state.name}")
