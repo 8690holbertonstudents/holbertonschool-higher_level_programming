52f8865920b0e75f83da8d3d22e4c1518205c9bf
8690holbertonstudents <8690@holbertonstudents.com>
2024-06-22 11:45:58 +0200

    SQL ORM - correction (5) T7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 """
-    Script that lists all State objects from the database hbtn_0e_6_usa
-
+Python module that lists all State objects from the database hbtn_0e_6_usa
+Usinng module SQLAlchemy
+Import State and Base from the model_state module
 """
-
 if __name__ == "__main__":
+    from sqlalchemy import create_engine
     from model_state import Base, State
-    from sqlalchemy import (create_engine)
     from sqlalchemy.orm import sessionmaker
     import sys
 
-    # Address of the data base URL
-    DATABASE_URL = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        sys.argv[1], sys.argv[2], sys.argv[3])
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
 
-    # Connection through the database with engine module
-    engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-
-    # Creation of a session
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    connection_string = (
+        "mysql+mysqlconnector://{}:{}@localhost:3306/{}".format(usr, pwd, db))
+    engine = create_engine(connection_string, echo=True)
 
     Base.metadata.create_all(engine)
 
-    # Find all results
-    states = session.query(State).all()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+    states = session.query(State).order_by(State.id.asc()).all()
+
+    for row in states:
+        print("{}: {}".format(row.id, row.name))
 
     session.close()
+    engine.dispose()
