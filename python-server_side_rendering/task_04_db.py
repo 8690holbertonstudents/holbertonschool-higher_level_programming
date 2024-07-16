a26dd1641a59cba8d953ff18de907e3ad5ebc38d
8690holbertonstudents <8690@holbertonstudents.com>
2024-07-16 10:58:38 +0200

    Python SSR - T4

---
+++ python-server_side_rendering/task_04_db.py
@@ -0,0 +1,77 @@
+"""
+"""
+from flask import Flask, render_template, request
+import json
+import csv
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/items')
+def items():
+    """
+    """
+    try:
+        with open('items.json', 'r', encoding='utf-8') as json_file:
+            data = json.load(json_file)
+            items = data.get("items", [])
+    except Exception as e:
+        items = []
+        return str(e)
+    return render_template('items.html', items=items)
+
+
+@app.route('/products')
+def products():
+    """
+    """
+    source = request.args.get('source')
+    id = request.args.get('id')
+
+    try:
+        if source == 'json':
+            with open('products.json', 'r', encoding='utf-8') as json_file:
+                products = json.load(json_file)
+        elif source == 'csv':
+            with open('products.csv', 'r', encoding='utf-8') as csv_file:
+                reader = csv.DictReader(csv_file)
+                products = list(reader)
+        elif source == 'sql':
+            db = sqlite3.connect('products.db')
+            db.row_factory = sqlite3.Row
+            res = db.execute('SELECT * FROM products')
+            products = res.fetchall()
+            db.close()
+        else:
+            return render_template('product_display.html', error='Wrong source')
+
+        if id:
+            for product in products:
+                if str(product['id']) == id:
+                    return render_template('product_display.html', product=product)
+            return render_template('product_display.html', error='Failed: Product not found')
+
+    except Exception as e:
+        products = []
+        return str(e)
+    return render_template('product_display.html', products=products)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
